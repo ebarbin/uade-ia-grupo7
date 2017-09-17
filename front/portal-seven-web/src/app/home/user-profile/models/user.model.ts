44b2994a33e340690d89bca21ced0d4edf4dae87
Emmanuel <emmanuel.barbin@gmail.com>
2017-09-17 12:33:35 -0300

    deploy changes

--- front/portal-seven-web/src/app/home/user-profile/models/user.model.ts
+++ front/portal-seven-web/src/app/home/user-profile/models/user.model.ts
@@ -5,6 +5,8 @@ export class User {
     constructor(
         public id:number,
         public userName:string,
+        public sureName:string,
+        public email:string,
         public firstName:string,
         public password:string,
         public image:Image){}
