ebe235f94d9e080640e77187c5100948c50843a1
Emmanuel <emmanuel.barbin@gmail.com>
2017-09-26 18:39:38 -0300

    address model front

--- front/portal-seven-web/src/app/home/hotel-offer/models/hotel.model.ts
+++ front/portal-seven-web/src/app/home/hotel-offer/models/hotel.model.ts
@@ -1,3 +1,4 @@
+import { Address } from './address.model';
 import { Image } from '../../../shared/models/image.model';
 
 export class Hotel {
@@ -5,5 +6,6 @@ export class Hotel {
     constructor(
         public id: number, 
         public images: Image[], 
-        public name: string){}
+        public name: string,
+        public address:Address){}
 }
