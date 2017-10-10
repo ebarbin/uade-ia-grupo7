c2abea596d065ce2c9fc61f2ee3fd61b6f0f8783
gparra <gonza.parra@gmail.com>
2017-10-10 12:22:58 -0300

    search actualizado y nuevo hasQuota
    Cambio el query de search. Ahora valida correctamente la fecha de la quota. Si te piden una semana, la cuota debe estar disponible toda la semana, y lo que yo había hecho sólo validaba con que al menos uno de los días tuviese la capacidad pedida. Ya lo cambie y el hasQuota hace el mismo chequeo.

--- back/workspace/portal-seven-ejb/ejbModule/ar/edu/uade/ia/ejbs/HotelOfferEJB.java
+++ back/workspace/portal-seven-ejb/ejbModule/ar/edu/uade/ia/ejbs/HotelOfferEJB.java
@@ -37,32 +37,39 @@ public class HotelOfferEJB {
 		Date dateTo = request.getToDate();
 		Integer roomQty = request.getRoomQuantity();
 		Integer peoplePerRoom = request.getPeoplePerRoom();
-
+		Integer difDates = 0;
+		
 		// Quota cuyo dia este dentro del rango del filtro
 		// Quota cuyo hotel de su oferta coincida con el hotel del filtro
 		// Quota cuyo cupo diario supere o iguale la cantidad de habitaciones del filtro
 		// Quota cuya capacidad de personas de habitacion de la oferta sea superior o
 		// igual a cantidad de personas del filtro
+		// Debe haber al menos 1 Quota por día en el período del filtro
+		
 		StringBuffer queryBuilder = new StringBuffer("select ofe");
-		queryBuilder.append(" from Quota as quo");
-		queryBuilder.append(" inner join quo.offer as ofe");
+		queryBuilder.append(" from HotelOffer as ofe");
 		queryBuilder.append(" inner join ofe.hotel ho");
 		queryBuilder.append(" inner join ofe.room ro");
 		queryBuilder.append(" where 1 = 1");
+		queryBuilder.append(" and ( select count(quo)");
+		queryBuilder.append("		from Quota as quo ");
+		queryBuilder.append("		where quo.offer.id = ofe.id");
+		
+		if (dateFrom != null && dateTo != null) {
+			queryBuilder.append(" 	and quo.quotaDate between :dateFrom and :dateTo");
+			difDates = (int) ((dateTo.getTime()-dateFrom.getTime())/86400000);
+			difDates += 1;
+		}
+		if (roomQty != null)
+			queryBuilder.append(" 	and quo.availableQuota >= :roomQty");
+		
+		queryBuilder.append("     ) = :difDates");
 		
 		if (idHotel != null)
 			queryBuilder.append(" and ho.id = :idHotel");
-		if (dateFrom != null && dateTo != null)
-			queryBuilder.append(" and quo.quotaDate between :dateFrom and :dateTo");
-		if (dateFrom != null && dateTo == null)
-			queryBuilder.append(" and quo.quotaDate >= :dateFrom");
-		if (dateFrom == null && dateTo != null)
-			queryBuilder.append(" and quo.quotaDate <= :dateTo");
-		if (roomQty != null)
-			queryBuilder.append(" and quo.availableQuota >= :roomQty");
 		if (peoplePerRoom != null)
 			queryBuilder.append(" and ro.capacity >= :peoplePerRoom");
-
+			
 		Query query = this.em.createQuery(queryBuilder.toString());
 		if (idHotel != null)
 			query.setParameter("idHotel", idHotel);
@@ -72,18 +79,46 @@ public class HotelOfferEJB {
 			query.setParameter("peoplePerRoom", peoplePerRoom);
 		if (dateFrom != null && dateTo != null) {
 			query.setParameter("dateFrom", dateFrom, TemporalType.DATE);
-			query.setParameter("dateTo", dateTo, TemporalType.DATE);
-		} else if (dateFrom != null && dateTo == null) {
-			query.setParameter("dateFrom", dateFrom, TemporalType.DATE);
-		} else if (dateFrom == null && dateTo != null) {
-			query.setParameter("dateTo", dateTo, TemporalType.DATE);
+			query.setParameter("dateTo", dateTo, TemporalType.DATE);			
 		}
-		
+		query.setParameter("difDates", difDates.longValue());
+
 		return query.getResultList();
 	}
 
 	public Boolean hasQuota(Integer hotelOfferId, HotelOfferRequestDTO filter) {
-		return Boolean.TRUE;
+		
+
+		Date dateFrom = filter.getFromDate();
+		Date dateTo = filter.getToDate();
+		Integer roomQty = filter.getRoomQuantity();
+		int value = 0;
+		
+		// Quota cuyo dia este dentro del rango del filtro
+		// Quota cuyo hotel de su oferta coincida con el hotel del filtro
+		// Quota cuyo cupo diario supere o iguale la cantidad de habitaciones del filtro
+		// Quota cuya capacidad de personas de habitacion de la oferta sea superior o
+		// igual a cantidad de personas del filtro
+		StringBuffer queryBuilder = new StringBuffer("select quo");
+		queryBuilder.append(" from Quota as quo");
+		queryBuilder.append(" where quo.id = :hotelOfferId");
+		queryBuilder.append(" and ( select count(quo)");
+		queryBuilder.append("		from Quota as quo ");
+		queryBuilder.append("		where quo.offer.id = ofe.id");
+		queryBuilder.append(" 		and quo.quotaDate between :dateFrom and :dateTo");
+		queryBuilder.append(" 		and quo.availableQuota >= :roomQty");		
+		queryBuilder.append("     ) = :difDates");
+		
+		Query query = this.em.createQuery(queryBuilder.toString());
+			
+		query.setParameter("dateFrom", dateFrom, TemporalType.DATE);
+		query.setParameter("dateTo", dateTo, TemporalType.DATE);
+		query.setParameter("hotelOfferId", hotelOfferId);
+		query.setParameter("roomQty", roomQty);
+		
+		value = query.getFirstResult();
+		
+		return (value ==1);
 	}
 	
 	public HotelOffer getDetail(Integer id) {
