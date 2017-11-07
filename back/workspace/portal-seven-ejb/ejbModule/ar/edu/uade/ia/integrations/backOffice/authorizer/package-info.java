93d2eb58c649c39c311671a973ec1da3ac9dae3d
Emmanuel <emmanuel.barbin@gmail.com>
2017-11-06 21:24:36 -0300

    .

--- back/workspace/portal-seven-ejb/ejbModule/ar/edu/uade/ia/integrations/backOffice/authorizer/package-info.java
+++ back/workspace/portal-seven-ejb/ejbModule/ar/edu/uade/ia/integrations/backOffice/authorizer/package-info.java
@@ -1,2 +1,2 @@
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://soap.servicios/")
-package ar.edu.uade.ia.integrations.backOffice.authorization;
+package ar.edu.uade.ia.integrations.backOffice.authorizer;
